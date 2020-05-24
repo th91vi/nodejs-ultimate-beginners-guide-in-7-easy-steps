f9c22f68295167cc08c39385104ed025fc4f8a78
Thiago Vieira <th91v@outlook.com>
2020-05-23 23:39:46 -0300

    inicio do projeto, primeiros experimentos com eventEmitter

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log('hello! 👋🏼');
+
+// // global em Node eh similar a Window no browser
+// console.log(global.luckyNum);
+
+// global.luckyNum = '23';
+
+// console.log(global.luckyNum);
+
+const myModule = require('./my-module');
+
+console.log(myModule);
